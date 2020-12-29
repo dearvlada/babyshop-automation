e5d37db0d5fe7af08dd2d8fa0b965bb06de90753
Vlada <vlada.tk.il@gmail.com>
2020-12-29 16:47:11 +0200

    fullversion

--- 2test_busket.py
+++ 2test_busket.py
@@ -13,11 +13,24 @@ class NavigationTest(BaseCase):
     def test_basket(self):
         # go to the goods page
         self.get(self.base_url + 'edushape/s/2003')
+        # saving the name of 1st position
+        item_name = self.get_text('//article[1]/div[@class="card__content"]/h3/a/strong')
         # click on the 1st position
         self.click('//article[1]')
         # add to cart
         self.click('//button[@class="product-form__btn btn"]')
         # checkout
         self.click('//a[@class="btn product-form__btn--go-to-cart"]')
-        #
-        self.sleep(3)
+        # saving the name in the basket
+        basket_name = self.get_text('//div[2]/a[1]/span[2]')
+        # names should be matching
+        self.assert_equal(item_name,basket_name)
+        # delete the position
+        self.click('//a[2]/span')
+        self.sleep(1)
+        # getting the message that cart is empty
+        empty_text = self.get_text('//div/header/h1')
+        # text sould be matching
+        self.sleep(1)
+        self.assert_equal(empty_text, "Your cart is empty")
+        self.sleep(1)
